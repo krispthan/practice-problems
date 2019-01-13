4faf64c3b08628518e5efd105d7d0320766a798e
krispthan <44071872+krispthan@users.noreply.github.com>
2019-01-12 19:42:27 -0800

    worked on reverseArray

--- practice07/answer.js
+++ practice07/answer.js
@@ -6,18 +6,70 @@ and move it to the back of the word
 add 'ay' to the end of the word .push()
 or use  
 */
-var myString = "Hello my name is Stu";
 
-function changeToPigLatin(str){
-        var splitStr = str.split(' ');// "hello", "my", "name", "is", "stu";
-    for(var i=0; i< str.length; i++){
-        var currentIndex= str[i];
-        var firstChar = str[i].charAt(0);
-        if(firstChar){
-            str[i].shift();
-           str[i].push('ay');
-        }
+
+// function changeToPigLatin(str){
+//         var splitStr = str.split(' ');// "hello", "my", "name", "is", "stu";
+//     for(var i=0; i< splitStr.length; i++){
+//         var charToAdd= 'ay'
+//         var currentIndex= splitStr[i];
+//        var takeFirstChar= currentIndex.substr(1);//ello
+//        //add the first char to the back of 
+//        var addCharToBack =t
+//      var finalWord=  takeFirstChar = takeFirstChar + charToAdd;//o/elloay 
+//      finalWord+= takeFirstChar;
+//     //    myNewString.push(takeFirstChar);
+//     //    myNewString.join(" "
+      
+//         }
+//         return finalWord;
+//     }
+    
+  
+
+
+//create an array of the words
+//iterate through each character of the word 
+// create a container that stores the individual first character to be taken off //shift
+//take that same character and push it to the back of the word 
+//add "ay"to the back of the word replace method
+// combine the characters back into a string 
+
+// var myString = "Hello my name is Stu";
+
+// function piglatin(str){
+//     var strSplit = str.split(' ');
+//     for( var i = 0; i< strSplit.length; i++){
+//       for(var j =0; j< strSplit[i].length; j++){
+//             var currentIndex= strSplit[j];
+//             var currentIndex = 
+      
+//       }
+
+   
+//     }
+
+// }
+// console.log(piglatin(myString));
+
+//Reverse Array 
+var sentence = "Howdy, how are you?"
+function reverseStringInArray (string){
+  var returnString = "";
+    for(var i=0; i<string.length; i++){
+      var splitString= string.split(" ");
     }
-    return str[i];
-}
-console.log(changeToPigLatin(myString));
+    for(var j=0; j<splitString.length;j++){
+      var currentIndex= splitString[j];
+      var splitWord= currentIndex.split("");
+      var reverseString = splitWord.reverse();
+      returnString += reverseString.join("").toString() + " ";  
+    }
+   return returnString;
+  }
+
+  reverseStringInArray(sentence);
+
+console.log(reverseStringInArray(sentence));
+
+
